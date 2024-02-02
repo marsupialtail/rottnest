0af81d6d8b7550b29343074ad8cefa6c4e067bcb
Rain Jiang <rain-jiang@outlook.com>
2024-02-01 16:38:53 -0800

    fix the latest tantivy and the test dependencies

--- src/lib.rs
+++ src/lib.rs
@@ -1,40 +1,33 @@
 use pyo3::exceptions::PyValueError;
-use pyo3::types::PyString;
 use pyo3::prelude::*;
+use pyo3::types::PyString;
 
-use arrow::array::{make_array, Array, ArrayAccessor, ArrayData, StringArray, UInt64Array};
+use arrow::array::{make_array, Array, ArrayData, StringArray, UInt64Array};
 use arrow::error::ArrowError;
-use arrow::ffi_stream::ArrowArrayStreamReader;
-use arrow::pyarrow::{FromPyArrow, PyArrowException, PyArrowType, ToPyArrow};
-use arrow::record_batch::RecordBatch;
-use arrow::record_batch::{RecordBatchIterator, RecordBatchReader};
 
-use tantivy::schema::*;
+use arrow::pyarrow::{FromPyArrow, PyArrowException};
+
 use tantivy::tokenizer::*;
-// use tantivy_tokenizer_api::Tokenizer as TantivyApiTokenizerTrait;
 use tantivy_jieba::JiebaTokenizer;
 
 use std::collections::BTreeMap;
 use std::collections::HashMap;
 
 use bincode;
-use serde::{Deserialize, Serialize};
-use std::fs::{metadata, File};
+use std::fs::File;
 use std::io::{BufRead, BufReader, Cursor, Read, Seek, SeekFrom, Write};
 use zstd::stream::encode_all;
 use zstd::stream::read::Decoder;
-use zstd::stream::write::Encoder;
 
 use regex::Regex;
-use whatlang::{detect, Info, Lang, Script};
+use whatlang::{detect, Lang};
 
 use anyhow::Result;
 use lazy_static::lazy_static;
 use opendal::raw::oio::ReadExt;
 use opendal::services::Fs;
 use opendal::services::S3;
-use opendal::Metadata;
-use opendal::{Builder, Operator, Reader};
+use opendal::{Operator, Reader};
 use std::env;
 
 fn to_py_err(err: ArrowError) -> PyErr {
@@ -68,8 +61,8 @@ lazy_static! {
 impl TokenizerEnum {
     fn token_stream<'a>(&'a mut self, text: &'a str) -> tantivy::tokenizer::BoxTokenStream<'a> {
         match self {
-            TokenizerEnum::Simple(tokenizer) => tokenizer.token_stream(text).into(),
-            TokenizerEnum::Jieba(tokenizer) => tokenizer.token_stream(text).into(),
+            TokenizerEnum::Simple(tokenizer) => BoxTokenStream::new(tokenizer.token_stream(text)),
+            TokenizerEnum::Jieba(tokenizer) => BoxTokenStream::new(tokenizer.token_stream(text)),
             TokenizerEnum::English(tokenizer) => tokenizer.token_stream(text),
         }
     }
@@ -78,7 +71,7 @@ impl TokenizerEnum {
 /*
 Structure of the lava file
 It is important to put the posting lists first. Just trust me bro.
-| compressed posting lists line by line | compressed term dictionary | compressed posting list offsets| 
+| compressed posting lists line by line | compressed term dictionary | compressed posting list offsets|
 8 bytes = offsets of compressed term dict | 8 bytes = offset of compressed posting list offsets
 */
 
@@ -89,7 +82,6 @@ fn build_lava_natural_language(
     array: &PyAny,
     uid: &PyAny,
     language: Option<&PyAny>,
-    py: Python,
 ) -> PyResult<()> {
     let array = make_array(ArrayData::from_pyarrow(array)?);
     let uid = make_array(ArrayData::from_pyarrow(uid)?);
@@ -203,18 +195,22 @@ fn build_lava_natural_language(
 }
 
 #[tokio::main]
-async fn hoa(condensed_lava_file: &str, operator: &mut Operator, lava_files: Vec<&str>) -> Result<()> // hawaiian for lava condensation
+async fn hoa(
+    condensed_lava_file: &str,
+    operator: &mut Operator,
+    lava_files: Vec<&str>,
+) -> Result<()> // hawaiian for lava condensation
 {
     // instantiate a list of readers from lava_files
     let mut readers: Vec<Reader> = Vec::with_capacity(lava_files.len());
-    let mut decompressed_term_dictionaries: Vec<BufReader<Cursor<Vec<u8>>>> = Vec::with_capacity(lava_files.len());
+    let mut decompressed_term_dictionaries: Vec<BufReader<Cursor<Vec<u8>>>> =
+        Vec::with_capacity(lava_files.len());
     let mut file_sizes: Vec<u64> = Vec::with_capacity(lava_files.len());
     let mut plist_offsets: Vec<Vec<u64>> = Vec::with_capacity(lava_files.len());
 
     // read in and decompress all the term dictionaries in memory. The term dictionaries corresponding to English language should be small.
 
     for file in lava_files {
-        
         let file_size: u64 = operator.stat(file).await?.content_length();
         let mut reader: Reader = operator.clone().reader(file).await?;
         reader.seek(SeekFrom::End(-16)).await?;
@@ -225,24 +221,37 @@ async fn hoa(condensed_lava_file: &str, operator: &mut Operator, lava_files: Vec
         reader.read(&mut buffer[..]).await?;
         let compressed_plist_offsets_offset = u64::from_le_bytes(buffer);
 
-        reader.seek(SeekFrom::Start(compressed_term_dict_offset)).await?;
-        let mut compressed_term_dictionary: Vec<u8> = vec![0u8; (compressed_plist_offsets_offset - compressed_term_dict_offset) as usize];
+        reader
+            .seek(SeekFrom::Start(compressed_term_dict_offset))
+            .await?;
+        let mut compressed_term_dictionary: Vec<u8> =
+            vec![0u8; (compressed_plist_offsets_offset - compressed_term_dict_offset) as usize];
         reader.read(&mut compressed_term_dictionary[..]).await?;
         let mut decompressed_term_dictionary: Vec<u8> = Vec::new();
-        let mut decompressor: Decoder<'_, BufReader<&[u8]>> = Decoder::new(&compressed_term_dictionary[..])?;
+        let mut decompressor: Decoder<'_, BufReader<&[u8]>> =
+            Decoder::new(&compressed_term_dictionary[..])?;
         decompressor.read_to_end(&mut decompressed_term_dictionary)?;
         let cursor = Cursor::new(decompressed_term_dictionary);
         let buf_reader: BufReader<Cursor<Vec<u8>>> = BufReader::new(cursor);
 
-        reader.seek(SeekFrom::Start(compressed_plist_offsets_offset)).await?;
-        let mut buffer2: Vec<u8> = vec![0u8; (file_size - compressed_plist_offsets_offset - 16) as usize];
+        reader
+            .seek(SeekFrom::Start(compressed_plist_offsets_offset))
+            .await?;
+        let mut buffer2: Vec<u8> =
+            vec![0u8; (file_size - compressed_plist_offsets_offset - 16) as usize];
         reader.read(&mut buffer2).await?;
         decompressor = Decoder::new(&buffer2[..])?;
-        let mut decompressed_serialized_plist_offsets: Vec<u8> = Vec::with_capacity(buffer2.len() as usize);
+        let mut decompressed_serialized_plist_offsets: Vec<u8> =
+            Vec::with_capacity(buffer2.len() as usize);
         decompressor.read_to_end(&mut decompressed_serialized_plist_offsets)?;
-        let this_plist_offsets: Vec<u64> = bincode::deserialize(&decompressed_serialized_plist_offsets)
-            .map_err(|e| pyo3::exceptions::PyValueError::new_err(format!("Bincode deserialization error: {}", e)))?;
-    
+        let this_plist_offsets: Vec<u64> =
+            bincode::deserialize(&decompressed_serialized_plist_offsets).map_err(|e| {
+                pyo3::exceptions::PyValueError::new_err(format!(
+                    "Bincode deserialization error: {}",
+                    e
+                ))
+            })?;
+
         plist_offsets.push(this_plist_offsets);
         decompressed_term_dictionaries.push(buf_reader);
         file_sizes.push(file_size);
@@ -267,7 +276,6 @@ async fn hoa(condensed_lava_file: &str, operator: &mut Operator, lava_files: Vec
     new_plist_offsets.push(0);
 
     while current_lines.iter().any(Option::is_some) {
-
         // Find the smallest current line
         let smallest_line = current_lines
             .iter()
@@ -275,7 +283,7 @@ async fn hoa(condensed_lava_file: &str, operator: &mut Operator, lava_files: Vec
             .min()
             .unwrap()
             .clone(); // Find the minimum line
-    
+
         term_dictionary += &smallest_line;
         term_dictionary += "\n";
 
@@ -287,22 +295,28 @@ async fn hoa(condensed_lava_file: &str, operator: &mut Operator, lava_files: Vec
             if current_lines[i].is_some() {
                 let line = current_lines[i].as_ref().unwrap();
                 if line.eq(&smallest_line) {
-
                     // we need to read and decompress the plists
                     let offset = plist_offsets[i][plist_cursor[i] as usize];
-                    let size = plist_offsets[i][plist_cursor[i] as usize + 1] - plist_offsets[i][plist_cursor[i] as usize];
+                    let size = plist_offsets[i][plist_cursor[i] as usize + 1]
+                        - plist_offsets[i][plist_cursor[i] as usize];
                     let file_reader = &mut readers[i];
                     file_reader.seek(SeekFrom::Start(offset as u64)).await?;
                     let mut compressed_plist = vec![0u8; size as usize];
                     file_reader.read(&mut compressed_plist).await?;
                     let mut decompressor = Decoder::new(&compressed_plist[..])?;
-                    let mut decompressed_serialized_plist: Vec<u8> = Vec::with_capacity(compressed_plist.len() as usize);
+                    let mut decompressed_serialized_plist: Vec<u8> =
+                        Vec::with_capacity(compressed_plist.len() as usize);
                     decompressor.read_to_end(&mut decompressed_serialized_plist)?;
-                    let mut this_plist: Vec<u64> = bincode::deserialize(&decompressed_serialized_plist)
-                        .map_err(|e| pyo3::exceptions::PyValueError::new_err(format!("Bincode deserialization error: {}", e)))?;
-                   
+                    let mut this_plist: Vec<u64> =
+                        bincode::deserialize(&decompressed_serialized_plist).map_err(|e| {
+                            pyo3::exceptions::PyValueError::new_err(format!(
+                                "Bincode deserialization error: {}",
+                                e
+                            ))
+                        })?;
+
                     plist.append(&mut this_plist);
-        
+
                     plist_cursor[i] += 1;
 
                     let reader = &mut decompressed_term_dictionaries[i];
@@ -318,9 +332,9 @@ async fn hoa(condensed_lava_file: &str, operator: &mut Operator, lava_files: Vec
         // print out plist
         let serialized = bincode::serialize(&plist).unwrap();
         let compressed_plist = encode_all(&serialized[..], 0).expect("Compression failed");
-        new_plist_offsets.push(new_plist_offsets[new_plist_offsets.len() - 1] + compressed_plist.len() as u64);
+        new_plist_offsets
+            .push(new_plist_offsets[new_plist_offsets.len() - 1] + compressed_plist.len() as u64);
         output_file.write_all(&compressed_plist)?;
-        
     }
 
     println!("{:?}", term_dictionary);
@@ -343,20 +357,30 @@ async fn hoa(condensed_lava_file: &str, operator: &mut Operator, lava_files: Vec
 }
 
 #[pyfunction]
-fn merge_lava(condensed_lava_file: &PyString, lava_files: Vec<&PyString>) -> PyResult<()> 
-{
-    // you should only merge them on local disk. It's not worth random accessing S3 for this because of the request costs. 
+fn merge_lava(condensed_lava_file: &PyString, lava_files: Vec<&PyString>) -> PyResult<()> {
+    // you should only merge them on local disk. It's not worth random accessing S3 for this because of the request costs.
     // worry about running out of disk later. Assume you have a fast SSD for now.
     let mut builder = Fs::default();
     let current_path = env::current_dir()?;
     builder.root(current_path.to_str().expect("no path"));
-    let mut operator = Operator::new(builder).map_err(|e| pyo3::exceptions::PyValueError::new_err(format!("Fs Builder construction error: {}", e)))?.finish();
+    let mut operator = Operator::new(builder)
+        .map_err(|e| {
+            pyo3::exceptions::PyValueError::new_err(format!("Fs Builder construction error: {}", e))
+        })?
+        .finish();
 
-    let result = hoa(condensed_lava_file.to_str()?, &mut operator, lava_files.into_iter().map(|x| x.to_str().unwrap()).collect::<Vec<&str>>());
+    let result = hoa(
+        condensed_lava_file.to_str()?,
+        &mut operator,
+        lava_files
+            .into_iter()
+            .map(|x| x.to_str().unwrap())
+            .collect::<Vec<&str>>(),
+    );
 
-    Ok(result.map_err(|e| pyo3::exceptions::PyValueError::new_err(format!("merging error: {}", e)))?)
+    Ok(result
+        .map_err(|e| pyo3::exceptions::PyValueError::new_err(format!("merging error: {}", e)))?)
 }
-
 
 #[tokio::main]
 async fn search_lava_async(operator: &mut Operator, file: &str, query: &str) -> Result<Vec<u64>> {
@@ -375,13 +399,15 @@ async fn search_lava_async(operator: &mut Operator, file: &str, query: &str) -> 
     // now read the term dictionary
     let mut compressed_term_dictionary: Vec<u8> =
         vec![0u8; (compressed_plist_offsets_offset - compressed_term_dictionary_offset) as usize];
-    reader.seek(SeekFrom::Start(compressed_term_dictionary_offset)).await?;
+    reader
+        .seek(SeekFrom::Start(compressed_term_dictionary_offset))
+        .await?;
     reader.read(&mut compressed_term_dictionary[..]).await?;
 
     let mut decompressed_term_dictionary: Vec<u8> = Vec::new();
     let mut decompressor: Decoder<'_, BufReader<&[u8]>> =
         Decoder::new(&compressed_term_dictionary[..])?;
-    decompressor.read_to_end(&mut decompressed_term_dictionary)?;    
+    decompressor.read_to_end(&mut decompressed_term_dictionary)?;
 
     let cursor = Cursor::new(decompressed_term_dictionary);
     let buf_reader: BufReader<Cursor<Vec<u8>>> = BufReader::new(cursor);
@@ -402,8 +428,11 @@ async fn search_lava_async(operator: &mut Operator, file: &str, query: &str) -> 
     }
 
     // seek to the offset
-    reader.seek(SeekFrom::Start(compressed_plist_offsets_offset)).await?;
-    let mut buffer2: Vec<u8> = vec![0u8; (file_size - compressed_plist_offsets_offset - 16) as usize];
+    reader
+        .seek(SeekFrom::Start(compressed_plist_offsets_offset))
+        .await?;
+    let mut buffer2: Vec<u8> =
+        vec![0u8; (file_size - compressed_plist_offsets_offset - 16) as usize];
     reader.read(&mut buffer2).await?;
     decompressor = Decoder::new(&buffer2[..])?;
     let mut decompressed_serialized_plist_offsets: Vec<u8> =
@@ -413,12 +442,13 @@ async fn search_lava_async(operator: &mut Operator, file: &str, query: &str) -> 
         .map_err(|e| {
             pyo3::exceptions::PyValueError::new_err(format!("Bincode deserialization error: {}", e))
         })?;
-    
+
     // now read the plist offsets that you need, whose indices are in matched
 
     let mut plist_result: Vec<u64> = Vec::new();
     for i in matched {
-        reader.seek(SeekFrom::Start(plist_offsets[i as usize]))
+        reader
+            .seek(SeekFrom::Start(plist_offsets[i as usize]))
             .await?;
         let mut buffer3: Vec<u8> =
             vec![0u8; (plist_offsets[(i + 1) as usize] - plist_offsets[i as usize]) as usize];
@@ -489,7 +519,7 @@ fn search_lava(file: &str, query: &str) -> PyResult<Vec<u64>> {
 
 /// This module is a python module implemented in Rust.
 #[pymodule]
-fn rottnest_rs(py: Python, m: &PyModule) -> PyResult<()> {
+fn rottnest_rs(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(build_lava_natural_language, m)?)?;
     m.add_function(wrap_pyfunction!(search_lava, m)?)?;
     m.add_function(wrap_pyfunction!(merge_lava, m)?)?;
