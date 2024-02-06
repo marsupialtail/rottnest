3dc0736a5bb3ce8c1b833716909cd88d9aa531d4
Tony Wang <zihengw@stanford.edu>
2024-02-05 16:19:01 -0800

    update test

--- test.py
+++ test.py
@@ -13,21 +13,28 @@ def basic_test():
 
 def merge_test():
 
-    a = polars.from_dict({"a":["a","c", "e"]}).to_arrow()
+    a = polars.from_dict({"a":["a","d", "h"]}).to_arrow()
     b = polars.from_dict({"a":["cn","en", "bump"]}).to_arrow()
     text = a["a"].combine_chunks().cast(pyarrow.string())
     uid = pyarrow.array([1,2,3]).cast(pyarrow.uint64())
     language = b["a"].combine_chunks().cast(pyarrow.string())
     print(rottnest_rs.build_lava_natural_language("1.lava", text, uid, language))
 
-    a = polars.from_dict({"a":["b","d", "f"]}).to_arrow()
-    b = polars.from_dict({"a":["cn","en", "bump"]}).to_arrow()
+    a = polars.from_dict({"a":["b","e"]}).to_arrow()
+    b = polars.from_dict({"a":["cn","en"]}).to_arrow()
     text = a["a"].combine_chunks().cast(pyarrow.string())
-    uid = pyarrow.array([4,5,6]).cast(pyarrow.uint64())
+    uid = pyarrow.array([4,5]).cast(pyarrow.uint64())
     language = b["a"].combine_chunks().cast(pyarrow.string())
     print(rottnest_rs.build_lava_natural_language("2.lava", text, uid, language))
 
-    rottnest_rs.merge_lava("merged.lava", ["1.lava", "2.lava"])
+    a = polars.from_dict({"a":["c","f"]}).to_arrow()
+    b = polars.from_dict({"a":["cn","en"]}).to_arrow()
+    text = a["a"].combine_chunks().cast(pyarrow.string())
+    uid = pyarrow.array([7,8]).cast(pyarrow.uint64())
+    language = b["a"].combine_chunks().cast(pyarrow.string())
+    print(rottnest_rs.build_lava_natural_language("3.lava", text, uid, language))
+
+    rottnest_rs.merge_lava("merged.lava", ["1.lava", "2.lava", "3.lava"])
 
     print(rottnest_rs.search_lava("merged.lava", "d"))
 
