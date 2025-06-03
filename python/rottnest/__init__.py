72fd2d1e191d46349e8702272e20203c47428b1a
Ziheng Wang <tonyw@anthropic.com>
2025-06-02 17:28:24 -0700

    refactored backend

--- python/rottnest/__init__.py
+++ python/rottnest/__init__.py
@@ -1,5 +1,7 @@
 import rottnest as rottnest
-from .backends import iceberg
+from .backends.utils import  get_fs_from_file_path, get_physical_layout, group_mergeable_indices, index_files, merge_indices, search_index, search_parquet_lake
+from .backends.iceberg import IcebergBackend
+import rottnest.backends.s3_utils as s3_utils
 from .indices.index_interface import RottnestIndex
 from .indices.bm25_index import Bm25Index
 from .indices.substring_index import SubstringIndex
